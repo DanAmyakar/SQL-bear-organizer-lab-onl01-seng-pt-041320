50654d83bdc0f1d16c3096468ad6344874344511
Daniel Amyakar <dan.rockit@gmail.com>
2020-06-11 14:07:00 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,7 +20,7 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
   "SELECT color, COUNT(color) AS my_count
-  "
+  GROUP BY color "
 end
 
 def counts_number_of_bears_with_goofy_temperaments
